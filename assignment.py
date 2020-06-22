26a8b7424371aac16f4a64e37e40b1e373b9b1c9
kingbosser <wangpengyu_0123@163.com>
2020-06-22 21:43:10 +0800

    提交作业

--- assignment.py
+++ assignment.py
@@ -1,8 +1,24 @@
 from adapter import StreamAdapter
+from typing import Dict, Hashable, Iterable
+import json
 
 
 class CourseWareB(StreamAdapter):
-    pass
+
+	@classmethod
+	def load_state(cls, raw_state: str) -> Hashable:
+		state = json.loads(raw_state).get("commonComponentState")
+		panel_state = (0, 0, 0, 0)
+		if state is not None:
+			if "4cb5f12f9e164c6c545a55202bc818f2" in state:
+			    panel_state = tuple(state["4cb5f12f9e164c6c545a55202bc818f2"]["answer"])
+		return panel_state
+
+	@classmethod
+	def right_ans(cls, stream: Iterable) -> bool:
+		right_ans = (1, 2, 0, 3)
+		return tuple(stream) == right_ans
+
 """
 在下面定义CourseWareB
 答案状态在commonComponentState下的4cb5f12f9e164c6c545a55202bc818f2下的answer字段
@@ -10,7 +26,39 @@ class CourseWareB(StreamAdapter):
 """
 
 if __name__ == "__main__":
-    """
-    在这里处理日志输出，输出结果为result.csv，三个字段为：学生ID，状态，是否为正确状态
-    """
-    pass
+	"""
+	在这里处理日志输出，输出结果为result.csv，三个字段为：学生ID，状态，是否为正确状态
+	"""
+	
+	# 读取data.csv
+	f = open("./data.csv",encoding="utf-8")
+	lines = f.readlines()
+	# 结果文件内容拼接
+	string = "uid" + "\t" + "state" + "\t" + "is_right" + "\n"
+	# 正确答案
+	right_answer = [1, 2, 0, 3]
+	for i in range(len(lines)):
+		# 跳过第一行表头
+		if i == 0:
+			pass
+		else:
+			# 获取每一个字段
+			arr = lines[i].split("\t")
+			uid = arr[0]
+			cr_code = arr[1]
+			trace_id = arr[2]
+			state = arr[3]
+			ctime_ts = arr[4]
+			# 处理state字段
+			currentstate = json.loads(state).get("commonComponentState")["4cb5f12f9e164c6c545a55202bc818f2"]["answer"]
+			is_right = currentstate == right_answer
+			# 拼接内容
+			string += str(uid) + "\t" + str(currentstate) + "\t" + str(is_right) + "\n"
+	f.close()
+	# 写文件
+	fw = open("./result.csv","w",encoding="utf-8")
+	fw.write(string)
+	fw.close()
+	
+			
+
--- tests/test_assignment.py
+++ tests/test_assignment.py
@@ -6,12 +6,28 @@ class AssignmentTest(unittest.TestCase):
     """
     这两个测试必写，你也可以增加其他你认为重要的测试
     """
-
     def test_load_state(self):
-        CourseWareB.load_raw_state("")
+    	# GIVEN
+        raw_state = {
+            "commonComponentState": {
+                "4cb5f12f9e164c6c545a55202bc818f2": {"answer": [1, 2, 0, 3],},
+            },
+        }
+        # WHEN
+        result = CourseWareB.load_raw_state(json.dumps(raw_state))
+        # THEN
+        target_state = (1, 2, 0, 3)
+        self.assertEqual(target_state, result)
 
     def test_right_ans(self):
-        CourseWareB.is_user_right(())
+    	# GIVEN
+        stream = [1, 2, 0, 3]
+        # THEN
+        self.assertTrue(CourseWareB.is_user_right(stream))
+
+        stream = [2, 1, 1, 3]
+        # THEN
+        self.assertTrue(CourseWareB.is_user_right(stream))      
 
 
 if __name__ == "__main__":
